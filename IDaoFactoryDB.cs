c69784f0378bf5b6f5b42f974fb411bd5ea1efdf
Diego Gutierrez <diegoalfonsogutierrez@gmail.com>
2019-01-06 00:51:35 -0500

    Añadido Factory para dao de bases de dato y la factory para daos de mysql

---
+++ IDaoFactoryDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ING_service
+{
+    public interface IDaoFactoryDB
+    {
+        IDao<Juego> CrearDaoJuego();
+        IDao<Conjunto> CrearDaoConjunto();
+        IDao<TipoJuego> CrearTipoJuego();
+    }
+}
---
+++ MySqlDaoFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ING_service
+{
+    public class MySqlDaoFactory : IDaoFactoryDB
+    {
+        public IDao<Conjunto> CrearDaoConjunto()
+        {
+            return new MySqlDaoConjunto();
+        }
+
+        public IDao<Juego> CrearDaoJuego()
+        {
+            return new MySqlDaoJuego();
+        }
+
+        public IDao<TipoJuego> CrearTipoJuego()
+        {
+            return new MySqlDaoTipoJuego();
+        }
+    }
+}
