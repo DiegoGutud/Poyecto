1666b02df1cd20e9e4e78e9bacfc92e83777aa8e
DiegoGutud <40840277+DiegoGutud@users.noreply.github.com>
2019-01-05 21:55:11 -0500

    Add files via upload

---
+++ IDao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ING_service
+{
+    public interface IDao<T>
+    {
+        T Agregar(T entidad);
+
+        T Modifcar(T entidad);
+
+        void Eliminar(T entidad);
+
+        T Consultar(T entidad);
+
+        List<T> ConsultarTodos();
+        
+    }
+}
